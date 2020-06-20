f47c12d8129851c0f1fb95a19969224439b365d0
Intan Nora Syahira Binti Nazri <intan.norasyahira@outlook.com>
2020-06-20 19:43:30 +0800

    add latest database

--- bbisv2.sql
+++ bbisv2.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 13, 2020 at 02:51 PM
--- Server version: 10.1.32-MariaDB
--- PHP Version: 7.2.5
+-- Generation Time: Jun 20, 2020 at 01:42 PM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -36,7 +36,7 @@ CREATE TABLE `bloodbag` (
   `donorID` int(11) NOT NULL,
   `retrievedDate` date NOT NULL,
   `expiryDate` date NOT NULL,
-  `status` varchar(10) NOT NULL DEFAULT 'In Stock',
+  `status` varchar(15) NOT NULL DEFAULT 'In Stock',
   `dateCreated` datetime NOT NULL,
   `lastEdited` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -46,7 +46,11 @@ CREATE TABLE `bloodbag` (
 --
 
 INSERT INTO `bloodbag` (`id`, `productID`, `displayBloodbagID`, `bloodType`, `donorID`, `retrievedDate`, `expiryDate`, `status`, `dateCreated`, `lastEdited`) VALUES
-(1, 6, 'RBC600001', 'O', 2, '2020-01-07', '2020-02-11', 'In Stock', '2020-01-09 09:36:13', '2020-01-09 09:36:13');
+(1, 10, 'RBC1000001', 'A', 2, '2020-06-02', '2020-07-07', 'Discarded', '2020-06-02 01:56:16', '2020-06-02 01:56:16'),
+(2, 10, 'RBC1000002', 'B', 3, '2020-03-30', '2020-06-04', 'Discarded', '2020-06-04 10:33:41', '2020-06-07 19:04:15'),
+(3, 10, 'RBC1000003', 'O', 2, '2020-06-04', '2020-07-09', 'Discarded', '2020-06-04 12:11:58', '2020-06-04 12:11:58'),
+(4, 11, 'RBC1100004', 'B', 3, '2020-06-12', '2020-07-17', 'Transferred', '2020-06-12 09:06:39', '2020-06-12 09:06:39'),
+(5, 11, 'RBC1100005', 'B', 3, '2020-06-12', '2020-07-17', 'In Stock', '2020-06-12 09:07:04', '2020-06-12 09:07:04');
 
 -- --------------------------------------------------------
 
@@ -68,10 +72,9 @@ CREATE TABLE `components` (
 --
 
 INSERT INTO `components` (`id`, `name`, `shelfLife`, `prefix`, `dateCreated`, `lastEdited`) VALUES
-(1, 'Red Blood Cell', 35, 'RBC', '2019-09-19 22:48:03', '2019-09-19 22:48:03'),
-(2, 'Plasma', 730, 'PLM', '2019-09-19 22:48:03', '2019-09-19 22:48:03'),
-(3, 'Platelet', 5, 'PLT', '2019-09-19 22:48:03', '2019-09-19 22:48:03'),
-(6, 'Component A', 3, 'CPA', '2019-09-21 21:24:27', '2019-09-21 21:24:27');
+(4, 'Red Blood Cell', 35, 'RBC', '2020-05-31 20:39:49', '2020-05-31 20:41:40'),
+(5, 'Plasma', 730, 'PLM', '2020-05-31 20:40:31', '2020-05-31 20:40:31'),
+(6, 'Platelet', 5, 'PLT', '2020-05-31 20:40:49', '2020-05-31 20:40:49');
 
 -- --------------------------------------------------------
 
@@ -98,7 +101,8 @@ CREATE TABLE `donors` (
 
 INSERT INTO `donors` (`id`, `name`, `icNumber`, `gender`, `birthDate`, `bloodType`, `phoneNumber`, `emailAddress`, `dateRegistered`, `lastUpdated`) VALUES
 (2, 'Kim Yoohyeon', '970107143245', 'Female', '1997-01-07', 'O', '0192049259', 'yoohyeon97@dreamcatchercompany.co.kr', '2019-10-17 07:47:12', '2019-10-24 06:30:20'),
-(3, 'Choi Yuna', '971004085202', 'Female', '1997-10-04', 'B', '0132904590', 'yuju@sourcemusic.co.kr', '2019-10-24 05:33:53', '2019-10-24 05:33:53');
+(3, 'Choi Yuna', '971004085202', 'Female', '1997-10-04', 'B', '0132904590', 'yuju@sourcemusic.co.kr', '2019-10-24 05:33:53', '2019-10-24 05:33:53'),
+(4, 'Intan Nora Syahira Binti Nazri', '951209092119', 'Female', '1995-08-02', 'B', '0172125507', 'intan.norasyahira@outlook.com', '2020-06-16 00:52:50', '2020-06-16 00:55:33');
 
 -- --------------------------------------------------------
 
@@ -136,9 +140,26 @@ INSERT INTO `hospitals` (`id`, `hospitalName`, `address`, `town`, `state`, `post
 CREATE TABLE `notifications` (
   `id` int(11) NOT NULL,
   `productID` int(11) NOT NULL,
-  `bloodType` varchar(2) NOT NULL,
-  `minCount` int(11) NOT NULL
+  `typeAcount` int(11) NOT NULL DEFAULT '0',
+  `typeBcount` int(11) NOT NULL DEFAULT '0',
+  `typeOcount` int(11) NOT NULL DEFAULT '0',
+  `typeABcount` int(11) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `notifications`
+--
+
+INSERT INTO `notifications` (`id`, `productID`, `typeAcount`, `typeBcount`, `typeOcount`, `typeABcount`) VALUES
+(2, 10, 5, 5, 5, 5),
+(3, 11, 10, 5, 10, 5),
+(5, 13, 5, 5, 15, 5),
+(6, 14, 5, 5, 15, 5),
+(7, 15, 5, 5, 15, 5),
+(8, 16, 5, 5, 10, 5),
+(9, 17, 0, 0, 20, 0),
+(10, 18, 0, 0, 0, 0),
+(11, 19, 0, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -150,7 +171,6 @@ CREATE TABLE `products` (
   `id` int(11) NOT NULL,
   `product` varchar(255) NOT NULL,
   `componentID` int(11) NOT NULL,
-  `minCount` int(11) NOT NULL,
   `dateCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `lastEdited` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -159,17 +179,16 @@ CREATE TABLE `products` (
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `product`, `componentID`, `minCount`, `dateCreated`, `lastEdited`) VALUES
-(1, 'Cryoprecipitate', 2, 1, '2019-09-19 23:37:05', '2019-09-19 23:37:05'),
-(2, 'Cryosupernatant', 2, 1, '2019-09-19 23:37:05', '2019-09-19 23:37:05'),
-(3, 'Fresh Frozen Plasma', 2, 1, '2019-09-19 23:37:05', '2019-09-19 23:37:05'),
-(4, 'Apheresis Platelet', 3, 1, '2019-09-19 23:37:05', '2019-09-19 23:37:05'),
-(5, 'Random Platelet Concentrate', 3, 1, '2019-09-19 23:37:05', '2019-09-19 23:37:05'),
-(6, 'Buffy-Coat Poor Red Cell', 1, 5, '2019-09-19 23:37:05', '2019-09-21 21:23:37'),
-(7, 'Filtered Red Cell', 1, 1, '2019-09-19 23:37:05', '2019-09-19 23:37:05'),
-(8, 'Packed Red Cell', 1, 1, '2019-09-19 23:37:05', '2019-09-19 23:37:05'),
-(9, 'Whole Blood', 1, 1, '2019-09-19 23:37:05', '2019-09-19 23:37:05'),
-(11, 'Product B', 6, 7, '2019-09-21 22:02:26', '2019-09-21 22:02:26');
+INSERT INTO `products` (`id`, `product`, `componentID`, `dateCreated`, `lastEdited`) VALUES
+(10, 'Whole Blood', 4, '2020-05-31 20:54:57', '2020-05-31 20:58:26'),
+(11, 'Packed Red Cell', 4, '2020-05-31 20:56:04', '2020-05-31 21:01:15'),
+(13, 'Buffy-Coat Poor Red Cell', 4, '2020-05-31 21:00:38', '2020-05-31 21:04:02'),
+(14, 'Filtered Red Cell', 4, '2020-05-31 21:04:17', '2020-05-31 21:04:43'),
+(15, 'Fresh Frozen Plasma', 5, '2020-05-31 21:05:17', '2020-05-31 21:05:34'),
+(16, 'Cryosupernatant', 5, '2020-05-31 21:05:49', '2020-05-31 21:06:07'),
+(17, 'Cryoprecipitate', 5, '2020-05-31 21:06:20', '2020-05-31 21:06:40'),
+(18, 'Random Platelet Concentrate', 6, '2020-05-31 21:06:57', '2020-05-31 21:06:57'),
+(19, 'Apheresis Platelet', 6, '2020-05-31 21:07:09', '2020-05-31 21:07:09');
 
 -- --------------------------------------------------------
 
@@ -179,10 +198,12 @@ INSERT INTO `products` (`id`, `product`, `componentID`, `minCount`, `dateCreated
 
 CREATE TABLE `transactions` (
   `id` int(11) NOT NULL,
+  `displayTransactionID` varchar(6) NOT NULL,
   `userID` int(11) NOT NULL,
   `hospitalID` int(11) DEFAULT NULL,
   `bloodBagID` text NOT NULL,
   `transaction` varchar(255) NOT NULL,
+  `remarks` varchar(355) DEFAULT NULL,
   `dateCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -190,9 +211,14 @@ CREATE TABLE `transactions` (
 -- Dumping data for table `transactions`
 --
 
-INSERT INTO `transactions` (`id`, `userID`, `hospitalID`, `bloodBagID`, `transaction`, `dateCreated`) VALUES
-(1, 3, NULL, '', 'Discard Bloodbag', '2019-12-25 23:15:37'),
-(2, 3, NULL, '', 'Discard Bloodbag', '2019-12-27 23:56:28');
+INSERT INTO `transactions` (`id`, `displayTransactionID`, `userID`, `hospitalID`, `bloodBagID`, `transaction`, `remarks`, `dateCreated`) VALUES
+(1, 'T00001', 3, NULL, '[\"CPA1100007\"]', 'Discard Bloodbag', 'Wrong donor', '2020-04-09 00:06:35'),
+(2, 'T00002', 3, NULL, '[\"RBC600001\"]', 'Discard Bloodbag', 'Wrong info', '2020-04-09 00:07:31'),
+(3, 'T00003', 3, 2, '[\"PLT500008\",\"PLT500009\"]', 'Transfer Bloodbag', '', '2020-04-14 01:11:58'),
+(4, 'T00004', 3, NULL, '[\"RBC1000001\"]', 'Discard Bloodbag', 'Wrong info', '2020-06-04 12:11:08'),
+(5, 'T00005', 3, NULL, '[\"RBC1000003\"]', 'Discard Bloodbag', 'Wrong info', '2020-06-07 19:04:44'),
+(6, 'T00006', 3, NULL, '[\"RBC1000002\"]', 'Discard Bloodbag', 'Discard expired bloodbag', '2020-06-07 19:07:10'),
+(7, 'T00007', 3, 2, '[\"RBC1100004\"]', 'Transfer Bloodbag', '', '2020-06-12 09:08:44');
 
 -- --------------------------------------------------------
 
@@ -219,11 +245,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `name`, `icNumber`, `gender`, `phoneNumber`, `emailAddress`, `password`, `role`, `dateRegistered`, `lastUpdated`) VALUES
-(1, 'intan', 'Intan Nora Syahira Binti Nazri', '950802086206', 'Female', '0172125507', 'intan.norasyahira@abobloodcentre.com.my', '12345', 'admin', '2019-07-05 21:43:35', '2019-08-25 16:27:19'),
+(1, 'intan', 'Intan', '950510081995', 'Female', '0172125507', 'intan.norasyahira@abobloodcentre.com.my', 'admin', 'admin', '2019-07-05 21:43:35', '2020-05-29 22:13:13'),
 (2, 'yuju', 'Choi Yuna', '971004187281', 'Female', '0123456789', 'yuna_choi@abobloodcentre.com.my', 'yuju123', 'user', '2019-07-05 21:45:19', '2019-08-12 18:17:05'),
-(3, 'yoohyeon', 'Kim Yoohyeon', '970107172007', 'Female', '0142125507', 'yoohyeon_kim@abobloodcentre.com.my', 'yuju123', 'user', '2019-07-05 21:47:56', '2019-10-28 22:53:09'),
-(4, 'jiu', 'Kim Minji', '940521086206', 'Female', '0142441994', 'jiu@abobloodcentre.com.my', 'jiu123', 'user', '2019-08-09 09:19:45', '2019-08-12 18:17:05'),
-(5, 'sinb', 'Hwang Eunbi', '990620', 'Female', '0163661866', 'sinb@abobloodcentre.com.my', 'sinb123', 'user', '2019-08-09 09:23:20', '2019-08-12 18:17:05');
+(3, 'yoohyeon', 'Kim Yoohyeon', '970107172007', 'Female', '0142125507', 'yoohyeon_kim@abobloodcentre.com.my', 'yuju123', 'user', '2019-07-05 21:47:56', '2019-10-28 22:53:09');
 
 --
 -- Indexes for dumped tables
@@ -285,7 +309,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `bloodbag`
 --
 ALTER TABLE `bloodbag`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `components`
@@ -297,7 +321,7 @@ ALTER TABLE `components`
 -- AUTO_INCREMENT for table `donors`
 --
 ALTER TABLE `donors`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `hospitals`
@@ -309,25 +333,25 @@ ALTER TABLE `hospitals`
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
